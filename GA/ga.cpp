7d3d4eb897d6c80e4601c42f2b50626c439f2da3
Rafael <rafael.brum@ufv.br>
2018-07-26 00:08:16 -0300

    inicial

--- GA/ga.cpp
+++ GA/ga.cpp
@@ -2,15 +2,47 @@
 #include <algorithm>
 #include <time.h>
 #include <math.h>
-#include 'values.h'
+#include "values.h"
+#define nPop 41
+
+Values data;
+
+class Individuo {
+
+    int * pop;              // population vector
+    int no;                 //Number of depots
+    int nc;                 //Number of costumers
+    int n;                  //Total of depots and constumers
+
+public:
+
+    Individuo(){data.LoadBarreto("../Instances/21x5.dat");
+
+        pop = new int[nPop];    // start population vector with size nPop data.LoadBarreto("../Instances/21x5.dat");
+
+    }
+
+    int fitness(){              // calculate and return the fitness value of i individual
+        
+        return 0;
+
+    }
+    
+};    
 
 using namespace std;
 
 int main(){
+    
+    data.LoadBarreto("../Instances/21x5.dat");
+    Individuo pop;data.LoadBarreto("../Instances/21x5.dat");
 
 
-    // mudança para comitar
 
 
+    
+
+    
+    
     return 0;
 }
