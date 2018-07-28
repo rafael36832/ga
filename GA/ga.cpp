1ac04f4632728616910049d670af2e9aef1c27b6
Rafael <rafael.brum@ufv.br>
2018-07-27 22:57:21 -0300

    fitnessQuase

--- GA/ga.cpp
+++ GA/ga.cpp
@@ -9,14 +9,16 @@ Values data;
 
 class Individuo {
 
-    int * pop;              // population vector
-    int no;                 //Number of depots
-    int nc;                 //Number of costumers
-    int n;                  //Total of depots and constumers
-
 public:
-
-    Individuo(){data.LoadBarreto("../Instances/21x5.dat");
+    
+    int * pop;              // population vector
+    int fitnessValue = 0; 
+    int carroNow = 0; 
+    int depotNow = pop[0];
+    int wayNow = pop[0];
+    bool possible = true;
+    
+    Individuo(){
 
         pop = new int[nPop];    // start population vector with size nPop data.LoadBarreto("../Instances/21x5.dat");
 
@@ -24,7 +26,43 @@ public:
 
     int fitness(){              // calculate and return the fitness value of i individual
         
-        return 0;
+        for(int i=1; i<data.N; i++){
+
+            if(pop[i] == 0){    // zero 
+                
+                // aux
+
+                wayNow = depotNow;
+                carroNow = 0;
+
+                // fitness 
+
+                fitnessValue += data.FV; 
+
+
+            } else if(pop[i] < data.No){ // it's a depot
+
+                if (wayNow < data.No) {
+
+                    depotNow = pop[i]; 
+                    wayNow = pop[i];
+
+                } else { 
+                    
+                    fitnessValue += data.FD[depotNow];
+
+                    wayNow = pop[i];
+                    depotNow = pop[i]; 
+                    
+                }
+
+            } else {                // it's a costumerar
+
+                // verificar se o depot pode 
+                    // verificar se o carro pode                  
+            
+            }
+        }
 
     }
     
@@ -38,11 +76,6 @@ int main(){
     Individuo pop;data.LoadBarreto("../Instances/21x5.dat");
 
 
-
-
-    
-
-    
     
     return 0;
 }
